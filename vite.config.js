3c487e14ca0054c1b97155e1a0411cbd53ce50e9
Mrhamza01 <hamza.kamboh035@gmail.com>
2024-08-17 16:43:40 +0500

    update the vite confi

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 100000000
+    // build: {
+    //     chunkSizeWarningLimit: 100000000
+    // },
+    build:{
+      minify: true,
     },
-    base: "/3d-react-portfolio",
+    optimizeDeps: {
+      include: ['esm-dep > cjs-dep'],
+    },
+    base: "/",
 })
